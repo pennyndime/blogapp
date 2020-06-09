c23268a330b7be6e4ca4a8c609c1fc14e2e1c8db
pennyndime <teranpaul22@gmail.com>
2020-06-08 23:19:49 -0500

    Adding HTML responses to http

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const http = require('http')
+const fs = require('fs')
+const homePage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const notFoundPage = fs.readFileSync('notfound.html')
+
 const server = http.createServer((req, res) =>{
     if(req.url === '/about')
-        res.end('The About page')
+        res.end(aboutPage)
     else if(req.url === '/contact')
-        res.end('The Contact page')
+        res.end(contactPage)
     else if(req.url === '/')
-        res.end('The Home page')
+        res.end(homePage)
     else {
         res.writeHead(404)
-        res.end('Page not found')
+        res.end(notFoundPage)
     }
 })
 
