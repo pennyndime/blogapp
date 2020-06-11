89068bae11341bae5997394128f9aeb97446f3b1
pennyndime <teranpaul22@gmail.com>
2020-06-10 20:39:57 -0500

    Used Express to make handle and server responses, making node easier

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const http = require('http')
-const fs = require('fs')
-const homePage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notFoundPage = fs.readFileSync('notfound.html')
+// const http = require('http')
+// const fs = require('fs')
+// const homePage = fs.readFileSync('index.html')
+// const aboutPage = fs.readFileSync('about.html')
+// const contactPage = fs.readFileSync('contact.html')
+// const notFoundPage = fs.readFileSync('notfound.html')
+const express = require('express') //require express module
+const app = express() //calls express function to start new Express app
+const path = require('path')
+app.use(express.static('public'))
 
-const server = http.createServer((req, res) =>{
-    if(req.url === '/about')
-        res.end(aboutPage)
-    else if(req.url === '/contact')
-        res.end(contactPage)
-    else if(req.url === '/')
-        res.end(homePage)
-    else {
-        res.writeHead(404)
-        res.end(notFoundPage)
-    }
+app.listen(3000,()=>{
+    console.log("App listening on port 3000")
+})
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'index.html'))
 })
 
-server.listen(3000)
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'about.html'))
+})
+
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'contact.html'))
+})
+
+// const server = http.createServer((req, res) =>{
+//     if(req.url === '/about')
+//         res.end(aboutPage)
+//     else if(req.url === '/contact')
+//         res.end(contactPage)
+//     else if(req.url === '/')
+//         res.end(homePage)
+//     else {
+//         res.writeHead(404)
+//         res.end(notFoundPage)
+//     }
+// })
+
+// server.listen(3000)
